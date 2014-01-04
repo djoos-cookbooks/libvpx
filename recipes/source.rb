5f662dfc76bd838baa6ccf928c06fbaba3c57351
David Joos <david.joos@gmail.com>
2014-01-04 01:15:53 +0000

    Re-compile when source changes, fixes #1

--- recipes/source.rb
+++ recipes/source.rb
@@ -23,11 +23,17 @@ package yasm_package do
     action :upgrade
 end
 
+creates_libvpx = "#{node['libvpx']['prefix']}/bin/vpxenc"
+
+file "#{creates_libvpx}" do
+    action :nothing
+end
+
 git "#{Chef::Config['file_cache_path']}/libvpx" do
     repository node['libvpx']['git_repository']
     reference node['libvpx']['git_revision']
     action :sync
-    notifies :run, "bash[compile_libvpx]"
+    notifies :delete, "file[#{creates_libvpx}]", :immediately
 end
 
 bash "compile_libvpx" do
@@ -36,5 +42,5 @@ bash "compile_libvpx" do
         ./configure --prefix=#{node['libvpx']['prefix']}
         make clean && make && make install
     EOH
-    creates "#{node['libvpx']['prefix']}/bin/vpxenc"
+    creates "#{creates_libvpx}"
 end
