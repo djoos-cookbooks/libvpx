d127e3819200498cf55dc3c225943ed3b3cfd7e5
David Joos <david.joos@gmail.com>
2014-01-24 23:03:33 +0000

    Added name attribute in cookbook metadata, fixes #2

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
+name             "libvpx"
 maintainer       "David Joos"
 maintainer_email "development@davidjoos.com"
 license          "MIT"
 description      "Installs/Configures libvpx"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.1"
+version          "0.3.2"
 
 supports "ubuntu"
 
