40dc28dd9306adf98da19bbb824ad98a2f579032
Mark Desnoyer <desnoyer@neon-lab.com>
2014-07-31 11:06:40 -0700

    Added configure flags using the same template as other escapestudios cookbooks

--- recipes/source.rb
+++ recipes/source.rb
@@ -28,10 +28,23 @@ git "#{Chef::Config[:file_cache_path]}/libvpx" do
     notifies :delete, "file[#{creates_libvpx}]", :immediately
 end
 
+# Write the flags used to compile the application to Disk. If the flags
+# do not match those that are in the compiled_flags attribute - we recompile
+template "#{Chef::Config[:file_cache_path]}/libvpx-compiled_with_flags" do
+    source "compiled_with_flags.erb"
+    owner "root"
+    group "root"
+    mode 0600
+    variables(
+        :compile_flags => node['libvpx']['compile_flags']
+    )
+    notifies :delete, "file[#{creates_libvpx}]", :immediately
+end
+
 bash "compile_libvpx" do
     cwd "#{Chef::Config[:file_cache_path]}/libvpx"
     code <<-EOH
-        ./configure --prefix=#{node['libvpx']['prefix']}
+        ./configure --prefix=#{node['libvpx']['prefix']} #{node['libvpx']['compile_flags'].join(' ')}
         make clean && make && make install
     EOH
     creates "#{creates_libvpx}"
