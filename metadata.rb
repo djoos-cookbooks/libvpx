c7a1cd6698f492d38377109fd5ed9fe09cee46b4
David Joos <david.joos@gmail.com>
2014-08-02 21:04:02 +0100

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dev@escapestudios.com'
 license 'MIT'
 description 'Installs/Configures libvpx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.3.3'
+version '1.0.0'
 
 supports 'ubuntu'
 
